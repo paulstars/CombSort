2665886e84b4269a1d8529b86ac6c2169088db1c
Paul Steinke <paul.steinke2002@outlook.de>
2023-05-26 01:09:20 +0200

    Verbesserte Sout ausgabe von gap, index und swapped

--- src/CombSort.java
+++ src/CombSort.java
@@ -32,6 +32,7 @@ public class CombSort {
 
         // Wiederholt sich, solang gap nicht 1 ist und ein Wert getauscht wurde.
         while (gap != 1 || swapped) {
+            System.out.println("index" + "\t" + "gap" + " ");
 
             // Finde die nächste gap
             gap = getNextGap(gap);
@@ -41,7 +42,7 @@ public class CombSort {
 
             // Vergleiche alle Elemente mit gap
             for (int i = 0; i < n - gap; i++) {
-                System.out.println(i + "\t" + gap + "\n");
+                System.out.print(i + "\t\t" + gap + " ");
                 if (array[i] > array[i + gap]) {
 
                     // Wenn array[i] größer ist als array[i+gap], dann vertausche diese!
@@ -51,9 +52,19 @@ public class CombSort {
 
                     // Werte wurden vertauscht, daher wird swapped = true.
                     swapped = true;
-                    System.out.println("Swapped: "+ swapped + "\n");
+                    System.out.print("--> Swapped\n");
+                }else {
+                    System.out.print("\n");
                 }
             }
+            System.out.println("\n");
         }
     }
 }
+
+/*
+ * Dieser Code orientiert sich an dem von Rajat Mishra, unter:
+ * https://www.geeksforgeeks.org/comb-sort/
+ *
+ */
+
--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,9 @@ public class Main {
     public static void main(String[] args) {
         CombSort vortrag = new CombSort();
         int[] array = {3,14,4,6,9,12};
+        for (int j : array) System.out.print(j + " ");
+        System.out.println("\n Array wird sortiert! \n");
+
         vortrag.sort(array);
 
         System.out.println("\n Array wurde sortiert!");
