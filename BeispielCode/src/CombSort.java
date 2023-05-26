aff52db23cf717fb3139dff6dc962ab6e25bd691
Paul Steinke <96238837+paulstars@users.noreply.github.com>
2023-05-26 13:57:16 +0200

    update ComSort

--- BeispielCode/src/CombSort.java
+++ BeispielCode/src/CombSort.java
@@ -42,7 +42,7 @@ public class CombSort {
 
             // Vergleiche alle Elemente mit gap
             for (int i = 0; i < n - gap; i++) {
-                System.out.print(i + "\t\t" + gap + " ");
+                System.out.print(i + "\t" + gap + " ");
                 if (array[i] > array[i + gap]) {
 
                     // Wenn array[i] größer ist als array[i+gap], dann vertausche diese!
